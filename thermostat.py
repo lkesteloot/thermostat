64d9bb3ac16172f8648c03e585939c73bb447487
Lawrence Kesteloot <lk@teamten.com>
2020-03-06 05:13:19 +0000

    Add DB support.

--- thermostat.py
+++ thermostat.py
@@ -1,11 +1,20 @@
 import sys
 import time
 
+# sudo apt-get install sqlite3
+import sqlite3
+
 # sudo apt-get install python3-rpi.gpio
 import RPi.GPIO as GPIO
 
 # sudo apt-get install python3-smbus
 from smbus import SMBus
+
+HIST_LEN = 100
+
+MIN_TEMP = 60
+MAX_TEMP = 90
+DB_WRITE_PERIOD = 60
 
 HT16K33_DISPLAY_OFF = 0x80
 HT16K33_DISPLAY_ON = 0x81
@@ -33,6 +42,49 @@ NUMBERS = [
     0x6F, # 9
 ]
 
+# Query helpers.
+
+def query(conn, cmd, *parameters):
+    return list(conn.execute(cmd, *parameters))
+
+def query_single_row(conn, cmd, *parameters):
+    rows = query(conn, cmd, *parameters)
+    return rows[0] if rows else None
+
+def query_single_value(conn, cmd, *parameters):
+    row = query_single_row(conn, cmd, *parameters)
+    return row[0] if row is not None else None
+
+def db_upgrade0(conn):
+	conn.execute('''CREATE TABLE temp (
+		id INTEGER PRIMARY KEY,
+		actual_temp REAL,
+		set_temp REAL,
+		heater_on INTEGER,
+		recorded_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	)''')
+
+UPGRADES = [
+    db_upgrade0,
+]
+
+def upgrade_schema(conn):
+    conn.execute("CREATE TABLE IF NOT EXISTS schema_version (version INTEGER)")
+
+    current_version = query_single_value(conn, "SELECT version FROM schema_version ORDER BY version DESC LIMIT 1")
+    start_version = current_version + 1 if current_version is not None else 0
+
+    for upgrade_version in range(start_version, len(UPGRADES)):
+        print("Applying upgrade %d" % upgrade_version)
+        upgrade = UPGRADES[upgrade_version]
+        upgrade(conn)
+        conn.execute("INSERT INTO schema_version VALUES (?)", (upgrade_version,))
+
+    conn.commit()
+
+    print("There are %d entries in historical table" %
+	query_single_value(conn, "SELECT count(*) FROM temp"))
+
 def get_temp(bus):
 	x = bus.read_i2c_block_data(THERMOMETER_ADDRESS, 0x05, 2)
 	# Clear flags from the value
@@ -42,9 +94,7 @@ def get_temp(bus):
 		c = (x[0]*16 + x[1]/16.0) - 256
 	else:
 		c = x[0]*16 + x[1]/16.0
-	# print("Temperature: %.2f C" % c)
 	f = c*9/5 + 32
-	# print("Temperature: %.2f F" % f)
 	return f
 
 # We're on /dev/i2c-1
@@ -117,6 +167,8 @@ def cb(channel):
 		d = 1 if a ^ b else -1
 
 	rot_value += d
+	rot_value = max(rot_value, MIN_TEMP*4 - 2)
+	rot_value = min(rot_value, MAX_TEMP*4 - 2)
 
 	old_a = a
 	old_b = b
@@ -124,14 +176,40 @@ def cb(channel):
 GPIO.add_event_detect(ROT_A_PIN, GPIO.BOTH, callback=cb)
 GPIO.add_event_detect(ROT_B_PIN, GPIO.BOTH, callback=cb)
 
+conn = sqlite3.connect("data.db")
+upgrade_schema(conn)
+
 b = 15
 bus.write_i2c_block_data(SEVEN_SEGMENT_ADDRESS, HT16K33_BRIGHTNESS | b, [])
 
+db_write_time = time.time()
+
+hist = []
 while True:
-	actual_temp = int(get_temp(bus))
+	# Compute actual temperature.
+	f = get_temp(bus)
+	hist.append(f)
+	while len(hist) > HIST_LEN:
+		hist.pop(0)
+	actual_temp = sum(hist) / len(hist)
+
+	# Compute set temperature.
 	set_temp = (rot_value + 2) // 4
+
+	# Whether heater is on.
 	heater_on = actual_temp < set_temp
-	draw_value(actual_temp*100 + set_temp, heater_on)
+
+	# Record settings to the database.
+	now = time.time()
+	if now - db_write_time > DB_WRITE_PERIOD:
+		before = time.time()
+		conn.execute('''INSERT INTO temp (actual_temp, set_temp, heater_on) VALUES (?, ?, ?)''',
+			(actual_temp, set_temp, heater_on))
+		conn.commit()
+		after = time.time()
+		db_write_time = now
+
+	draw_value(int(actual_temp)*100 + set_temp, heater_on)
 	time.sleep(0.1)
 
 bus.close()
